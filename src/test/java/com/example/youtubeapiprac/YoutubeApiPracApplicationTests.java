a5e74a65d87d385d519b42b7af5ab0ae0e3b4cc9
Mo-Greene <13blueboy13@daum.net>
2023-07-20 23:02:48 +0900

    Initial commit

---
+++ src/test/java/com/example/youtubeapiprac/YoutubeApiPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youtubeapiprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeApiPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
